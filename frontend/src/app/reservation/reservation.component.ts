0b688196036a2c7e3a1c6a66ca437a14a30626a6
Giacomo <Giacomo@felsenstern.ch>
2021-09-30 14:15:47 +0200

    History Toggle in Reservations

--- frontend/src/app/reservation/reservation.component.ts
+++ frontend/src/app/reservation/reservation.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { KeycloakService } from 'keycloak-angular';
 import { Reservation } from '../models/Reservation';
 import { ReservationService } from '../services/reservation.service';
 
@@ -10,13 +13,32 @@ import { ReservationService } from '../services/reservation.service';
 
 export class ReservationComponent implements OnInit, OnDestroy {
   public reservations?: Reservation[];
-  public searchQuery?: string;
+  public withHistory: boolean = false;
 
-  constructor(private readonly reservationService: ReservationService) {
+  constructor(private readonly _reservationService: ReservationService, protected _keycloakAngular: KeycloakService, private _snackBar: MatSnackBar) {
   }
 
-  ngOnInit(): void {
-    this.reservationService.readReservations(this.searchQuery).subscribe(result => this.reservations = result);
+  async ngOnInit() {
+    this.backendCall;
+  }
+
+  public refresh(event: Boolean): void {
+    this.withHistory = !this.withHistory;
+    this.backendCall();
+  }
+
+  public async backendCall(){
+    const id_landlord = await this._keycloakAngular.getKeycloakInstance().subject;
+    if (id_landlord && id_landlord?.length > 0) {
+      const httpParams = new HttpParams().set('ID_Landlord', id_landlord).set('withHistory', this.withHistory) ;
+
+      this._reservationService.readReservations(httpParams).subscribe(result => {
+        this.reservations = result;
+      });
+    }
+    else {
+      this._snackBar.open('Die Reservationen konnten nicht geladen werden.', 'Schliessen');
+    }
   }
 
   ngOnDestroy(): void {
