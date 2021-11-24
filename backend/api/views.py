393a1d5cf6a2cc801e5021247ac04ac3981373c5
hanskeller <hans.keller@optimatik.ch>
2021-11-24 21:56:25 +0100

    chore: working on available endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 from django_filters import rest_framework
 from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from . import serializers, models
 
@@ -11,6 +13,22 @@ class ParkingViewSet(viewsets.ModelViewSet):
     queryset = models.Parking.objects.all()
     search_fields = ['Location']
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+
+    @action(methods=['get'], detail=False)
+    def available(self, request, pk=None):
+        location = self.request.query_params.get('location')
+        fromDT = self.request.query_params.get('from')
+        toDT = self.request.query_params.get('to')
+        # timezone.now().date()
+
+        if toDT:
+            objs = models.Parking.objects.all()
+        else:
+            objs = models.Parking.objects.all()
+
+        serializer = serializers.ParkingSerializer(objs, many=True)
+        return Response(serializer.data)
+
 
 class ReservationViewSet(viewsets.ModelViewSet):
     pagination_class = None
