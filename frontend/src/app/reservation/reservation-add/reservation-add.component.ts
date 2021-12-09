5187ac4e73a1c17fc74f60b99e59b849a119117c
Giacomo <Giacomo@felsenstern.ch>
2021-12-09 21:29:56 +0100

    ButtonCaption AddReservation

--- frontend/src/app/reservation/reservation-add/reservation-add.component.ts
+++ frontend/src/app/reservation/reservation-add/reservation-add.component.ts
@@ -6,18 +6,18 @@ import {
   ViewChild,
 } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { KeycloakService } from 'keycloak-angular';
 import * as moment from 'moment';
+import { BehaviorSubject } from 'rxjs';
 import { Globals } from 'src/app/globals';
 import { Parking } from 'src/app/models/Parking';
 import { Reservation } from 'src/app/models/Reservation';
 import { ReservationService } from 'src/app/services/reservation.service';
 import { dateBeforeValidator } from 'src/app/shared/common-validators/dateBefore-validators.directive';
 import { dateInPastValidator } from 'src/app/shared/common-validators/dateInPast-validators.directive';
-import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Component({
   selector: 'app-reservation-add',
@@ -31,6 +31,7 @@ export class ReservationAddComponent implements AfterContentInit {
   public reservation: Reservation;
   public reservationForm: FormGroup;
   public isServerPending: Boolean = false;
+  public buttonCaption$: BehaviorSubject<string> =  new BehaviorSubject<string>('wait and see ->');
 
   constructor(
     private _activatedRoute: ActivatedRoute,
@@ -48,7 +49,7 @@ export class ReservationAddComponent implements AfterContentInit {
       id: new FormControl(this.reservation.id),
       ID_Renter: new FormControl(this.reservation.ID_Renter),
       DateTimeFrom: new FormControl(this.reservation.DateTimeFrom, [Validators.required, dateInPastValidator]),
-      DateTimeTo: new FormControl(this.reservation.DateTimeTo, [Validators.required, dateInPastValidator]),
+      DateTimeTo: new FormControl(this.reservation.DateTimeTo, [Validators.required, dateInPastValidator,]),
       ID_Parking: new FormControl(this.reservation.ID_Parking?.id),
       IsCanceled: new FormControl(this.reservation.IsCanceled),
       Amount: new FormControl({value: this.reservation.Amount, disabled: true}),
@@ -69,13 +70,15 @@ export class ReservationAddComponent implements AfterContentInit {
   private onValueChanges(): void {
     this.reservationForm.controls['DateTimeFrom'].valueChanges.subscribe(
       (dateTimeFrom) => {
-        this.reservation.Amount = this.currencyRound (this.calculateDiff(dateTimeFrom, this.reservation.DateTimeTo));
+        this.reservation.Amount = this.getAmountByDateRange(dateTimeFrom, this.reservation.DateTimeTo);
+        this.buttonCaption$.next (this.getButtonCaption(dateTimeFrom, this.reservation.DateTimeTo));
       }
     );
 
     this.reservationForm.controls['DateTimeTo'].valueChanges.subscribe(
       (dateTimeTo) => {
-        this.reservation.Amount = this.currencyRound(this.calculateDiff(this.reservation.DateTimeFrom, dateTimeTo));
+        this.reservation.Amount = this.getAmountByDateRange(this.reservation.DateTimeFrom, dateTimeTo);
+        this.buttonCaption$.next (this.getButtonCaption(this.reservation.DateTimeFrom, dateTimeTo));
       }
     );
 
@@ -97,7 +100,7 @@ export class ReservationAddComponent implements AfterContentInit {
     this.reservation.DateTimeTo.setTime(this.reservation.DateTimeFrom.getTime() + (1 * 60 * 60 * 1000) /* plus eine Stunde */)
     this.reservation.PricePerHour = parking.PricePerHour ?? 1.0 ;
     this.reservation.IsCanceled = false;
-    this.reservation.Amount = this.currencyRound(this.calculateDiff(this.reservation.DateTimeFrom, this.reservation.DateTimeTo));
+    this.reservation.Amount = this.getAmountByDateRange(this.reservation.DateTimeFrom, this.reservation.DateTimeTo);
     this.reservationForm.reset(this.reservation);
   }
 
@@ -119,21 +122,10 @@ export class ReservationAddComponent implements AfterContentInit {
     }
   }
 
-  public onAddOrEdit(reservation: Reservation): void {
+  public onAdd(reservation: Reservation): void {
     this.isServerPending = true;
 
     reservation.DateTimeFrom = new Date(moment(reservation.DateTimeFrom).toDate());
-    if (reservation.id) {
-      this._reservationService
-        .updateReservation(this.reservation)
-        .subscribe((result) => {
-          if (result) {
-            this._router.navigate(['reservation']);
-          } else {
-            this.showError('Die Reservation konnte nicht gespeichert werden.');
-          }
-        });
-    } else {
       const ID_Renter = this._keycloakAngular.getKeycloakInstance().subject;
       if (ID_Renter) {
         reservation.ID_Renter = ID_Renter;
@@ -147,67 +139,44 @@ export class ReservationAddComponent implements AfterContentInit {
             }
           });
       }
-    }
   }
 
-  public deleteReservation(id: number): void {
-    const modalDialog = this.openModal();
-    modalDialog.afterClosed().subscribe((result) => {
-      if (result && id) {
-        this._reservationService
-          .deleteReservation(id)
-          .then((result) => {
-            console.log('delete', result);
-            if (result.status) {
-              this._router.navigate(['reservation']);
-            } else {
-              this.showError(result.message);
-            }
-          })
-          .catch((error) => {
-            this.showError(
-              'Die Reservation konnte nicht gelöscht werden, bitte versuchen sie es später erneut.'
-            );
-          });
-      }
-    });
-  }
-
-  public resetForm(): void {
-    this.reservationForm.reset();
-    this.reservationForm.markAsUntouched();
-  }
 
   public showError(content: string) {
     this._snackBar.open(content, 'Schliessen', { duration: 5000 });
   }
 
-  public calculateDiff(from: Date, to: Date): number {
-    const _from = new Date(from);
-    const _to = new Date(to);
 
-    const minutes = Math.floor(
-      (Date.UTC(_to.getFullYear(), _to.getMonth(), _to.getDate(), _to.getHours(),_to.getMinutes()) -
-        Date.UTC(_from.getFullYear(), _from.getMonth(), _from.getDate(), _from.getHours(), _from.getMinutes())) /
-        (1000 * 60 )
-    );
-    const pricePerMinute = this.reservation.PricePerHour / 60;
-    return pricePerMinute * minutes;
+  public getAmountByDateRange(from: Date, to: Date): number {
+    const minutes = this.getMinutesByDateRange(from, to);
+      const pricePerMinute = this.reservation.PricePerHour / 60;
+      return this.currencyRound(pricePerMinute * minutes);
   }
 
-  public openModal() {
-    const dialogConfig = new MatDialogConfig();
+  public getMinutesByDateRange(_from: Date, _to: Date): number {
+    const from = new Date(_from);
+    const to = new Date(_to);
+    return Math.floor(
+      (Date.UTC(to.getFullYear(),to.getMonth(), to.getDate(), to.getHours(),to.getMinutes()) -
+      Date.UTC(from.getFullYear(), from.getMonth(), from.getDate(), from.getHours(), from.getMinutes())) /
+      (1000 * 60 )
+    );
+  }
 
-    dialogConfig.disableClose = true;
-    dialogConfig.id = 'modal-component';
-    dialogConfig.height = '170px';
-    dialogConfig.width = '550px';
-    dialogConfig.data = {
-      title: 'Wollen sie die Reservation wirklich löschen?',
-      description: 'Dieser Vorgang kann nicht rückgängig gemacht werden.',
-    };
+  public getButtonCaption(from: Date, to: Date): string {
 
-    return this.matDialog.open(ModalComponent, dialogConfig);
+    const amount = this.getAmountByDateRange(from,to);
+    const minTotal = this.getMinutesByDateRange(from, to);
+    const hours = Math.floor(minTotal / 60);
+    console.log(minTotal);
+    const min = (minTotal - (hours * 60));
+    let result = `${min}min für ${amount.toFixed(2)} CHF ->`;
+
+    if (hours > 0) {
+      return `${hours}h ${result}`;
+    }
+
+    return result;
   }
 
   get f() {
