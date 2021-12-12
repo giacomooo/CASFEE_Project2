469eb513c4b5d71b16a0da136e0d130746ee756c
hanskellerost <hans.keller@ost.ch>
2021-12-12 13:33:12 +0100

    chore: background to webp, css optimations for background image

--- frontend/src/app/globals.ts
+++ frontend/src/app/globals.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class Globals {
-  isLoading = false;
+  set isLoading(arg: boolean) {
+    setTimeout(() => {
+      this.loading.next(arg);
+    });
+  }
+  get isLoading() {
+    return this.loading.value;
+  }
+  loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 }
